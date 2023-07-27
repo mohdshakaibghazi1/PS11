e542aab484909e871c3dd0a9304045b2f90fa442
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-07-27 22:52:07 +0530

    PS11

---
+++ DoublyLinkList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ConAppDoublyLList
+{
+    public class Linkedlist
+    {
+        public Node head;
+        public Linkedlist()
+        {
+            head = null;
+        }
+        public void PrintLlist()
+        {
+            Node temp = new Node();
+            temp = this.head;
+            if (temp != null)
+            {
+                Console.WriteLine("the List Contains: ");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("This DoublylList is empty");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConAppDoublyLList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist linkedlist = new Linkedlist();
+            Node first = new Node
+            {
+                data = 12,
+                next = null,
+                prev = null
+            };
+            linkedlist.head = first;
+            Node second = new Node
+
+            {
+                data = 20,
+                next = null,
+
+            };
+            first.next = second;
+            second.prev = first;
+            Node third = new Node
+            {
+                data = 30,
+                next = null
+            };
+            second.next = third;
+            third.prev = second;
+            linkedlist.PrintLlist();
+            Console.ReadKey();
+        }
+    }
+}
